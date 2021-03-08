0c92c9042daa74c8744449bca6691e663db5f9af
siamshaeed <siamshaeed@gmail.com>
2021-03-07 22:02:57 -0800

    include footer and cupon

---
+++ resources/views/frontEnd/includes/cupon.blade.php
@@ -0,0 +1,28 @@
+<div class="coupons">
+	<div class="container">
+		<div class="coupons-grids text-center">
+			<div class="col-md-3 coupons-gd">
+				<h3>Buy your product in a simple way</h3>
+			</div>
+			<div class="col-md-3 coupons-gd">
+				<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+				<h4>LOGIN TO YOUR ACCOUNT</h4>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
+				sit amet, consectetur.</p>
+			</div>
+			<div class="col-md-3 coupons-gd">
+				<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+				<h4>SELECT YOUR ITEM</h4>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
+				sit amet, consectetur.</p>
+			</div>
+			<div class="col-md-3 coupons-gd">
+				<span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>
+				<h4>MAKE PAYMENT</h4>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
+				sit amet, consectetur.</p>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
--- resources/views/frontEnd/master.blade.php
+++ resources/views/frontEnd/master.blade.php
@@ -791,107 +791,11 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 	</div>
 </div>
 <!-- //product-nav -->
+<!-- Cupon file include -->
+@include('frontEnd.includes.cupon')
+<!-- Footer file include -->
+@include('frontEnd.includes.footer')
 
-<div class="coupons">
-	<div class="container">
-		<div class="coupons-grids text-center">
-			<div class="col-md-3 coupons-gd">
-				<h3>Buy your product in a simple way</h3>
-			</div>
-			<div class="col-md-3 coupons-gd">
-				<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-				<h4>LOGIN TO YOUR ACCOUNT</h4>
-				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
-				sit amet, consectetur.</p>
-			</div>
-			<div class="col-md-3 coupons-gd">
-				<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-				<h4>SELECT YOUR ITEM</h4>
-				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
-				sit amet, consectetur.</p>
-			</div>
-			<div class="col-md-3 coupons-gd">
-				<span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>
-				<h4>MAKE PAYMENT</h4>
-				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
-				sit amet, consectetur.</p>
-			</div>
-			<div class="clearfix"> </div>
-		</div>
-	</div>
-</div>
-<!-- footer -->
-<div class="footer">
-	<div class="container">
-		<div class="col-md-3 footer-left">
-			<h2><a href="index.html"><img src="{{asset('public/frontEnd/images/logo3.jpg')}}" alt=" " /></a></h2>
-			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor
-				sit amet, consectetur, adipisci velit, sed quia non 
-				numquam eius modi tempora incidunt ut labore 
-			et dolore magnam aliquam quaerat voluptatem.</p>
-		</div>
-		<div class="col-md-9 footer-right">
-			<div class="col-sm-6 newsleft">
-				<h3>SIGN UP FOR NEWSLETTER !</h3>
-			</div>
-			<div class="col-sm-6 newsright">
-				<form>
-					<input type="text" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
-					<input type="submit" value="Submit">
-				</form>
-			</div>
-			<div class="clearfix"></div>
-			<div class="sign-grds">
-				<div class="col-md-4 sign-gd">
-					<h4>Information</h4>
-					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li><a href="mens.html">Men's Wear</a></li>
-						<li><a href="womens.html">Women's Wear</a></li>
-						<li><a href="electronics.html">Electronics</a></li>
-						<li><a href="codes.html">Short Codes</a></li>
-						<li><a href="contact.html">Contact</a></li>
-					</ul>
-				</div>
-				
-				<div class="col-md-4 sign-gd-two">
-					<h4>Store Information</h4>
-					<ul>
-						<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>Address : 1234k Avenue, 4th block, <span>Newyork City.</span></li>
-						<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>Email : <a href="mailto:info@example.com">info@example.com</a></li>
-						<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>Phone : +1234 567 567</li>
-					</ul>
-				</div>
-				<div class="col-md-4 sign-gd flickr-post">
-					<h4>Flickr Posts</h4>
-					<ul>
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b15.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b16.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b17.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b18.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b15.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b16.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b17.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b18.jpg')}}" alt=" " class="img-responsive" /></a></li>
-
-						<li><a href="single.html"><img src="{{asset('public/frontEnd/images/b15.jpg')}}" alt=" " class="img-responsive" /></a></li>
-					</ul>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-		<p class="copy-right">&copy 2021 Smart Shop. All rights reserved | Design by <a href="https://github.com/SiamShaeed">siamshaeed</a></p>
-	</div>
-</div>
-<!-- //footer -->
 <!-- login -->
 <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog" role="document">
